712b031830ad2c5337a81e8bcc7d479205503ca4
thesmiling <50331890+thesmiling@users.noreply.github.com>
2019-05-10 09:48:52 +0300

    b

--- dimasovbot.py
+++ dimasovbot.py
@@ -1,22 +1,12 @@
+# -*- coding: utf-8 -*-
+import config
 import telebot
-import bs4
-import perser
-import telebot
-from Task import Task
-import markups as m
 
+bot = telebot.TeleBot(config.token)
 
-TOKEN = "406374437:AAF96C4YUsV8_-SLsNTKDkSsp9AzxWS9o1c"
-bot = telebot. TeleBot (TOKEN)
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
 
-@bot.message_handler(commands=['start', 'go']) 
-def start_handler(message): bot.send_message(message.chat.id, 'Привет, когда я вырасту, я буду парсить заголовки с Хабра') 
-
-
-@bot.message_handler(content_types=['text']) 
-def text_handler(message): text = message.text.lower() chat_id = message.chat.id 
-if text == "привет": bot.send_message(chat_id, 'Привет, я бот - парсер хабра.') 
-elif text == "как дела?": bot.send_message(chat_id, 'Хорошо, а у тебя?') 
-else: bot.send_message(chat_id, 'Простите, я вам не понял :(')
-
-bot.polling(none_stop=True)
+if __name__ == '__main__':
+     bot.polling(none_stop=True)
