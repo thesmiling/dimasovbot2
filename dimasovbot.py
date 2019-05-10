ac6ee15f8801a85b3541fe03b37abd305506e0f0
thesmiling <50331890+thesmiling@users.noreply.github.com>
2019-05-10 11:26:10 +0300

    Update dimasovbot.py

--- dimasovbot.py
+++ dimasovbot.py
@@ -1,11 +1,20 @@
-import telebot
-
-TOKEN = '406374437:AAF96C4YUsV8_-SLsNTKDkSsp9AzxWS9o1c' 
-bot = telebot.TeleBot(TOKEN)
-
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
-
-if __name__ == '__main__':
-     bot.polling(none_stop=True)
+# Настройки
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+updater = Updater(token='406374437:AAF96C4YUsV8_-SLsNTKDkSsp9AzxWS9o1c') # Токен API к Telegram
+dispatcher = updater.dispatcher
+# Обработка команд
+def startCommand(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text='Привет, давай пообщаемся?')
+def textMessage(bot, update):
+    response = 'Получил Ваше сообщение: ' + update.message.text
+    bot.send_message(chat_id=update.message.chat_id, text=response)
+# Хендлеры
+start_command_handler = CommandHandler('start', startCommand)
+text_message_handler = MessageHandler(Filters.text, textMessage)
+# Добавляем хендлеры в диспатчер
+dispatcher.add_handler(start_command_handler)
+dispatcher.add_handler(text_message_handler)
+# Начинаем поиск обновлений
+updater.start_polling(clean=True)
+# Останавливаем бота, если были нажаты Ctrl + C
+updater.idle()
