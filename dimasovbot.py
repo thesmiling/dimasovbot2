94beca496e2fd65d9d7b21b0dca050c3251d5b7c
thesmiling <50331890+thesmiling@users.noreply.github.com>
2019-05-10 10:16:16 +0300

    h

--- dimasovbot.py
+++ dimasovbot.py
@@ -1,11 +1,7 @@
-# -*- coding: utf-8 -*-
-import config
 import telebot
 
 TOKEN = '406374437:AAF96C4YUsV8_-SLsNTKDkSsp9AzxWS9o1c' 
 bot = telebot.TeleBot(TOKEN)
-
-bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
